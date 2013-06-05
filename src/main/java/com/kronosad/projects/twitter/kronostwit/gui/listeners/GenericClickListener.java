a1ad1229f5caf1b949f5f30a11495925ea0f9732
Russell Richardson <Russell@KronosAD.com>
2013-06-05 18:02:16 -0500

    Added the ability to click links in a tweet!

---
+++ src/main/java/com/kronosad/projects/twitter/kronostwit/gui/listeners/GenericClickListener.java
@@ -0,0 +1,54 @@
+
+package com.kronosad.projects.twitter.kronostwit.gui.listeners;
+
+import java.awt.Desktop;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author Russell
+ */
+public class GenericClickListener extends MouseAdapter {
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Click called!");
+        if(e.getComponent() instanceof JMenuItem){
+            
+            JMenuItem item = (JMenuItem)e.getComponent();
+            
+            String[] words = item.getText().split(" ");
+            
+            for(String link : words){
+                if(link.contains("http")){
+                    try {
+                        URL url = new URL(link);
+                        try {
+                            Desktop.getDesktop().browse(url.toURI());
+                        } catch (IOException ex) {
+                            Logger.getLogger(GenericClickListener.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    } catch (URISyntaxException ex) {
+                        Logger.getLogger(GenericClickListener.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (MalformedURLException ex) {
+                        Logger.getLogger(GenericClickListener.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    
+}
