aef9072332d0a6d4a7f99c99e649c05a6ed0a874
Russell Richardson <Russell@KronosAD.com>
2013-11-18 23:21:31 -0600

    Okay that was an understatement. Now it works. For now.

--- src/main/java/com/kronosad/projects/twitter/kronostwit/gui/windows/Window.java
+++ src/main/java/com/kronosad/projects/twitter/kronostwit/gui/windows/Window.java
@@ -2,6 +2,7 @@ package com.kronosad.projects.twitter.kronostwit.gui.windows;
 
 import com.kronosad.projects.twitter.kronostwit.theme.ThemeDefault;
 import java.awt.Component;
+import java.awt.Container;
 import javax.swing.*;
 
 public abstract class Window extends JFrame{
@@ -22,18 +23,30 @@ public abstract class Window extends JFrame{
     }
 
     public void init(){
-        initFonts();
+        initFonts(this);
     }
     
-    protected void initFonts(){
-        for(Component component : this.getContentPane().getComponents()){
-            if(component instanceof JLabel){
-                component.setForeground(new ThemeDefault().getCurrentFontColor());
-            }
+    protected void initFonts(Component comp) {
+//        for(Component component : this.getContentPane().getComponents()){
+//            if(component instanceof JLabel){
+//                component.setForeground(new ThemeDefault().getCurrentFontColor());
+//            }
+//            
+//        }
+//        
+//        this.getContentPane().setBackground(new ThemeDefault().getCurrentColor());
+        
+        if (comp instanceof JLabel) {
+            
+            comp.setForeground(new ThemeDefault().getCurrentFontColor());
             
         }
-        
-        this.getContentPane().setBackground(new ThemeDefault().getCurrentColor());
+        if (comp instanceof Container) {
+            Component[] comps = ((Container) comp).getComponents();
+            for (int x = 0, y = comps.length; x < y; x++) {
+                initFonts(comps[x]);
+            }
+        }
         
         
     }
